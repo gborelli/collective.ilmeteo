eb94a062c7a1bf4441187f237369d375bdf393fa
Giorgio Borelli <giorgio@scaricchio.(none)>
2010-08-28 17:20:08 +0200

    first import

---
+++ README.txt
@@ -0,0 +1,7 @@
+Introduction
+============
+
+Integrate ilmeteo.it in Plone
+
+For further information see: http://www.ilmeteo.it/portale/servizi-web
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.ilmeteo',
+      version=version,
+      description="Tool to integrate ilmeteo.it in Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Giorgio Borelli',
+      author_email='giorgio@giorgioborelli.it',
+      url='http://svn.plone.org/svn/collective/collective.ilmeteo',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+      )
